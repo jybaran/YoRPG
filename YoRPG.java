14e8b3bf2e6143847f8175bdfc9a8a718c20cabf
mariavasilkin <m.vasilkin@gmail.com>
2013-11-17 15:56:16 -0500

    Little thing

--- YoRPG.java
+++ YoRPG.java
@@ -37,7 +37,7 @@ public class YoRPG {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Trying something for Rogue
     public int getMoveCounter (){
-    	return moveCounter;
+    	return moveCount;
     }
 
     // ~~~~~~~~~~~~~~ METHODS ~~~~~~~~~~~~~~~~~~~
