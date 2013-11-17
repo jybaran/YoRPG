e3598f3bcdb899f83466b3aa7d168c0da7706460
Jenny Baran <jenny.baran@gmail.com>
2013-11-17 15:01:08 -0500

    minor rogue mod
    matched to modded other classes

--- Rogue.java
+++ Rogue.java
@@ -1,6 +1,8 @@
-//Jenny Baran, Miranda Chaiken, Maria Vasilkin
-//HW26
-//2013.11.16
+// JENNY BARAN, MARIA VASILKIN, MIRANDA CHAIKEN
+// pd 8
+// HW26
+// 2013-11-15
+
 public class Rogue extends Character {
 
     public Rogue( String name ) {
@@ -18,12 +20,41 @@ public class Rogue extends Character {
         return super.attack(opponent);
     }
     
-    public void specialize() {}
-    public void normalize() {}
+    public void specialize() {
+	if (_attack < _altattack) {
+	    double tempA = _attack;
+	    _attack = _altattack;
+	    _altattack = tempA;
+	    int tempD = _def;
+	    _def = _altdef;
+	    _altdef = tempD;
+	}
+	/*if the attack stat is larger than the alt attack, the warrior is
+	  already prepped for a special attack, so nothing changes*/
+    }
+
+    public void normalize() {
+	if (_attack > _altattack) {
+	    double tempA = _attack;
+	    _attack = _altattack;
+	    _altattack = tempA;
+	    int tempD = _def;
+	    _def = _altdef;
+	    _altdef = tempD;
+	}
+	/*if the attack stat is smaller than the alt attack, the warrior is
+	  already prepped for a normal attack, so nothing changes*/
+    }
+
     public static String about() {
-        String retStr = "The wandering vagabond.Not that strong but resilient. ";
-	retStr += "/n Low strength, high defense";
+        String retStr = "The wandering vagabond. Not that strong but resilient. ";
+	retStr += "Low strength, high defense";
         return retStr;
+    }
+
+    public String ascii() {
+	String retStr = "";
+	return retStr;
     }
 
 }
